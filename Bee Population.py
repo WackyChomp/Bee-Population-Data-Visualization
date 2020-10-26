73ed62391147e576d588436c304e1a4251f2989c
Wilson Chan <wilsonchan54@gmail.com>
2020-10-26 12:45:55 -0400

    Creating application layout that contains html, dropdown, and graph

--- Bee Population.py
+++ Bee Population.py
@@ -19,7 +19,29 @@ df.reset_index(inplace = True)
 print(df[:5])
 
 #--------------------------------------------------#
+#Application Layout: Dash compenents / graphs / dropdown / checkbox / html
 
+app.layout = html.Div([
+    html.H1("Bee Web Application Dashboards with Dash", style = {'text-align': 'center'}),
 
+    dcc.Dropdown(id = "slct_year",
+        options = [
+            #users see the lable / dropdown looks at value
+            {"label": "2015", "value": 2015},
+            {"label": "2016", "value": 2016},
+            {"label": "2017", "value": 2017},
+            {"label": "2018", "value": 2018},
+            {"label": "2019", "value": 2019}],
+        multi = False,
+        value = 2015,
+        style = {'width': "40%"}
+        ),
+    
+    html.Div(id = 'output_container', children = []),
+
+    html.Br(),
+
+    dcc.Graph(id = 'bee_map', figure = {})
+])
 
 #--------------------------------------------------#
