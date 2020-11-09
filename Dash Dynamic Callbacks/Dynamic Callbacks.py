c9895d47af3240382f86218ec032c0eaa190f01b
Wilson Chan <wilsonchan54@gmail.com>
2020-11-08 23:58:05 -0500

    Initial Commit: importing the necessary modules

---
+++ Dash Dynamic Callbacks/Dynamic Callbacks.py
@@ -0,0 +1,8 @@
+import dash          #version 1.17.0
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, All, State, MATCh, ALLSMALLER
+import plotly.express as px
+import pandas as pd
+import numpy as np
+
