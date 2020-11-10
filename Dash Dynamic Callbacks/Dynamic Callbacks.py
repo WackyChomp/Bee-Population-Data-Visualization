05b582c6e652232aa19b4e92d99ad9c0f1bb4802
Wilson Chan <wilsonchan54@gmail.com>
2020-11-09 23:40:37 -0500

    Creating the application layout without adding the individual graphs or components

--- Dash Dynamic Callbacks/Dynamic Callbacks.py
+++ Dash Dynamic Callbacks/Dynamic Callbacks.py
@@ -12,3 +12,24 @@ df = pd.read_csv("India_Caste.csv")
 #rename column names
 df.rename(columns = {'under_trial': 'under trial' , 'state_name': 'state'}, inplace = True)
 
+app = dash.Dash(__name__)         #initiate the app
+
+#-------------------------------------#
+#Application Layout
+'''
+In basic callbacks, you must have the necessary components in layout before you introduce them in the callback
+
+All you need is the layout with the button and Div
+'''
+app.layout = html.Div([        
+    html.Div(children = [
+        html.Button('Add Chart', id = 'add-chart', n_clicks = 0),
+    ]),
+
+    html.Div(id = 'container')      #graphs and components stored in this Div with children array
+])
+
+#-------------------------------------#
+
+
+
